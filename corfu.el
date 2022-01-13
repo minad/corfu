2e90ff13242cc8a264cf13a777f1eed00936f0f8
Daniel Mendler <mail@daniel-mendler.de>
2022-01-13 21:16:50 +0100

    Add redisplay hack

--- corfu.el
+++ corfu.el
@@ -424,7 +424,11 @@ The completion backend can override this with
       (make-frame-invisible corfu--frame))
     (set-frame-position corfu--frame x y)
     (set-frame-size corfu--frame width height t)
-    (make-frame-visible corfu--frame)))
+    (unless (frame-visible-p corfu--frame)
+      ;; HACK: Force redisplay, otherwise the popup somtimes
+      ;; does not display content.
+      (redisplay)
+      (make-frame-visible corfu--frame))))
 
 (defun corfu--popup-show (pos off width lines &optional curr lo bar)
   "Show LINES as popup at POS - OFF.
