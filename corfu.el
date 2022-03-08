e2cc92b029bd9334f88d382def7066c35c31b3a9
Daniel Mendler <mail@daniel-mendler.de>
2022-03-08 11:07:03 +0100

    Version 0.20

--- corfu.el
+++ corfu.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler <mail@daniel-mendler.de>
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2021
-;; Version: 0.19
+;; Version: 0.20
 ;; Package-Requires: ((emacs "27.1"))
 ;; Homepage: https://github.com/minad/corfu
 
