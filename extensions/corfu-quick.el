e4c56b2482b2321a06f71951c85933f8702c907d
Daniel Mendler <mail@daniel-mendler.de>
2022-04-14 18:41:12 +0200

    corfu-quick: Reset echo message

--- extensions/corfu-quick.el
+++ extensions/corfu-quick.el
@@ -125,6 +125,8 @@ TWO is non-nil if two keys should be displayed."
 (defun corfu-quick-jump ()
   "Jump to candidate using quick keys."
   (interactive)
+  (setq corfu--echo-message "")
+  (corfu--echo-refresh)
   (if (= corfu--total 0)
       (and (message "No match") nil)
     (let ((idx (corfu-quick--read)))
