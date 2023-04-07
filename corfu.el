a122f0e991a32834e91610ce40bd637f73656da1
Daniel Mendler <mail@daniel-mendler.de>
2023-04-07 10:04:36 +0200

    Revert "Use frame-selected-window (See #316)"
    This reverts commit 449aa6a01f78e00faaff7ae52657106790400b7e.

--- corfu.el
+++ corfu.el
@@ -404,7 +404,11 @@ FRAME is the existing frame."
          (before-make-frame-hook)
          (after-make-frame-functions)
          (parent (window-frame)))
-    (unless (and (frame-live-p frame) (eq (frame-parent frame) parent))
+    (unless (and (frame-live-p frame)
+                 (eq (frame-parent frame) parent)
+                 ;; XXX HACK: It seems the frame can be alive but have a dead window?
+                 ;; Is this a Emacs 29 regression?
+                 (window-live-p (frame-root-window frame)))
       (when frame (delete-frame frame))
       (setq frame (make-frame
                    `((parent-frame . ,parent)
@@ -434,7 +438,7 @@ FRAME is the existing frame."
     (let ((new (face-attribute 'corfu-default :background nil 'default)))
       (unless (equal (frame-parameter frame 'background-color) new)
         (set-frame-parameter frame 'background-color new)))
-    (let ((win (frame-selected-window frame)))
+    (let ((win (frame-root-window frame)))
       (set-window-buffer win buffer)
       ;; Disallow selection of root window (#63)
       (set-window-parameter win 'no-delete-other-windows t)
@@ -471,7 +475,7 @@ FRAME is the existing frame."
   (when (and (frame-live-p frame) (frame-visible-p frame))
     (set-frame-parameter frame 'corfu--hide-timer nil)
     (make-frame-invisible frame)
-    (with-current-buffer (window-buffer (frame-selected-window frame))
+    (with-current-buffer (window-buffer (frame-root-window frame))
       (with-silent-modifications
         (erase-buffer)))))
 
