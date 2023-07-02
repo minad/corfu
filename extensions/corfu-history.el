664ef966f3668f3b5c30a8c5db429ae38ca00c61
Daniel Mendler <mail@daniel-mendler.de>
2023-07-02 10:12:29 +0200

    Version 0.37

--- extensions/corfu-history.el
+++ extensions/corfu-history.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2022
 ;; Version: 0.1
-;; Package-Requires: ((emacs "27.1") (corfu "0.36"))
+;; Package-Requires: ((emacs "27.1") (corfu "0.37"))
 ;; Homepage: https://github.com/minad/corfu
 
 ;; This file is part of GNU Emacs.
