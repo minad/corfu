a7fbcb6a2029790dd1b1bbc9c316a862152a0cd5
Daniel Mendler <mail@daniel-mendler.de>
2025-04-06 00:46:48 +0200

    Add corfu-history-duplicate

--- extensions/corfu-history.el
+++ extensions/corfu-history.el
@@ -49,6 +49,16 @@ or the property `history-length' of `corfu-history'.")
 (defvar corfu-history--hash nil
   "Hash table of Corfu candidates.")
 
+(defcustom corfu-history-duplicate 1.0
+  "Weight of duplicate elements, multiplied with `history-length'.
+Duplicate elements in the history are prioritized such that they appear
+earlier in the completion list.  The value should be between 0.0 and
+1.0. For 0 only the recency of history elements matters.  If the value
+is 1.0, frequency is more relevant than recency.  Note that
+`history-delete-duplicates' must be disabled for duplicates to appear."
+  :type 'float
+  :group 'corfu)
+
 (defun corfu-history--sort-predicate (x y)
   "Sorting predicate which compares X and Y."
   (or (< (cdr x) (cdr y))
@@ -58,13 +68,13 @@ or the property `history-length' of `corfu-history'.")
 (defun corfu-history--sort (cands)
   "Sort CANDS by history."
   (unless corfu-history--hash
-    (let ((ht (make-hash-table :test #'equal :size (length corfu-history))))
-      (cl-loop for elem in corfu-history for index from 0 do
-               (unless (gethash elem ht) (puthash elem index ht)))
-      (setq corfu-history--hash ht)))
-  (cl-loop for ht = corfu-history--hash for max = most-positive-fixnum
-           for cand on cands do
-           (setcar cand (cons (car cand) (gethash (car cand) ht max))))
+    (setq corfu-history--hash (make-hash-table :test #'equal :size (length corfu-history)))
+    (cl-loop for weight = (round (* history-length corfu-history-duplicate))
+             for elem in corfu-history for idx from 0
+             for dup = (gethash elem corfu-history--hash) do
+             (puthash elem (if dup (- dup weight) idx) corfu-history--hash)))
+  (cl-loop for max = most-positive-fixnum for cand on cands do
+           (setcar cand (cons (car cand) (gethash (car cand) corfu-history--hash max))))
   (setq cands (sort cands #'corfu-history--sort-predicate))
   (cl-loop for cand on cands do (setcar cand (caar cand)))
   cands)
